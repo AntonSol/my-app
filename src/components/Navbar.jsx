560f8b71609a75e7b41f708f8ff76eb19e71e14a
AntonS <solukov.anton@gmail.com>
2023-12-15 21:27:28 +0300

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Navbar'
+import './Navbar.module.css'
 const Navbar =()=>{
     return <nav className='nav'>
     <div className='item'>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import './Profile'
+import './Profile.module.css'
 const Profile =()=>{
     return <div className='content'>
     <img src='https://kartinki.pibig.info/uploads/posts/2023-04/thumbs/1680592686_kartinki-pibig-info-p-mnogo-kotov-na-odnoi-kartinke-arti-38.jpg'/>
+  <div>
+    ava+description
   </div>
+  <div>
+    <div>
+    My post
+  </div>
+    New post
+  </div>
+
+  </div>
+
 }
 export default Profile
