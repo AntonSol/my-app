b1a33a405bda0de365838b1a2f05f975b6c4b6b3
AntonS <solukov.anton@gmail.com>
2024-01-10 21:23:47 +0300

    пропсы

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from './MyPosts.module.css'
 import Posts from "./Post/Posts";
-const MyPosts =()=>{
+const MyPosts = () => {
     return <div>
-    My posts
-    <div>
-        New posts
-    </div>
-       <Posts />
-       <Posts />
-       <Posts />
+        My posts
+        <div>
+            New posts
+        </div>
+        <Posts
+            message='Hi are you? '
+            like={<div>5 лукасов</div>}
+        />
+        <Posts message='Its my first post '
+            like={<div>10 лукасов</div>} />
+        <Posts />
     </div>
 }
 export default MyPosts
--- src/components/Profile/MyPosts/Post/Posts.jsx
+++ src/components/Profile/MyPosts/Post/Posts.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './Posts.module.css'
-const Posts =()=>{
+const Posts =(props)=>{
     return ( 
     <div className={styles.item}>
      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTENrybzcoLwKmAYneHO7i8TCuBYBCOLAPm8EpnNaxXXZeI6MT9xLvx0FmomOMURbkEqUY&usqp=CAU"></img>
-        post 1
+       {props.message}
+       {props.like}
         </div>
     )
 }
