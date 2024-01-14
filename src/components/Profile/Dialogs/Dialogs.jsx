033819602249db15ec59a3fd812c08e7f5b6d602
AntonS <solukov.anton@gmail.com>
2024-01-14 11:43:34 +0300

    верстка страницы dialogs

--- src/components/Profile/Dialogs/Dialogs.jsx
+++ src/components/Profile/Dialogs/Dialogs.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 import styles from "./Dialogs.module.css"
-const Dialogs=()=>{
-    return(
-        <div>
-            Dialogs
+const Dialogs = () => {
+    return (
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    Anton1
+                </div>
+                <div className={styles.dialog}>
+                    Anton2
+                </div>
+                <div className={styles.dialog}>
+                    Anton3
+                </div>
+            </div>
+            <div className={styles.messages}>
+<div className={styles.message}>Hi</div>
+<div className={styles.message}>Hi i am Anton</div>
+<div className={styles.message}>Bye</div>
+            </div>
         </div>
     )
 }
