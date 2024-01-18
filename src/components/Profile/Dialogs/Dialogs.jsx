1d9e9f021f120b193534f80869b2706ed5ccc5ec
AntonS <solukov.anton@gmail.com>
2024-01-18 21:36:51 +0300

    1

--- src/components/Profile/Dialogs/Dialogs.jsx
+++ src/components/Profile/Dialogs/Dialogs.jsx
@@ -1,23 +1,30 @@
 import React from "react"
 import styles from "./Dialogs.module.css"
+import { NavLink } from "react-router-dom"
+
+const DialogItem = (props) => {
+    return (<div className={styles.dialogs}>
+        <NavLink path={'dialogs/*' + props.id}>{props.name}</NavLink>
+    </div>)
+
+}
+const Message = (props) => {
+    return (<div className={styles.dialogs}>{props.message}
+    </div>
+    )
+}
 const Dialogs = () => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
-                <div className={styles.dialog}>
-                    Anton1
-                </div>
-                <div className={styles.dialog}>
-                    Anton2
-                </div>
-                <div className={styles.dialog}>
-                    Anton3
-                </div>
+                <DialogItem name='Toha' id='0' />
+                <DialogItem name='Biba' id='1' />
+                <DialogItem name='Trololo' id='2' />
             </div>
             <div className={styles.messages}>
-<div className={styles.message}>Hi</div>
-<div className={styles.message}>Hi i am Anton</div>
-<div className={styles.message}>Bye</div>
+                <Message message='Hi' id='0' />
+                <Message message='I am Anton' id='1' />
+                <Message message='QQ' id='2' />
             </div>
         </div>
     )
