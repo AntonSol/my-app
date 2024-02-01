44b607bf6748545b71c07a8b4794d171ed6e61f7
AntonS <solukov.anton@gmail.com>
2024-02-01 20:03:01 +0300

    вынос данных в отдельный файл

---
+++ src/State.js
@@ -0,0 +1,17 @@
+let state={
+    posts:[
+        {id:0, message:'Hi are you? ', like:12},
+        {id:1, message:'Its my first post ', like:1},
+        {id:2, message:'YOU ', like:15},
+      ],
+    dialogsData:[
+        {id:0,name:'Toha'},
+        {id:1,name:'Biba'},
+        {id:2,name:'Trololo'},
+      ],
+    messagesData:[
+        {id:0,message:'Hi'},
+        {id:1,message:'I am Anton'},
+        {id:2,message:'QQ'},
+      ],
+}
