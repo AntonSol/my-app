5a7819bfb7fd302edc40b25e9e3445b0abf403eb
AntonS <solukov.anton@gmail.com>
2024-02-05 22:05:11 +0300

    callback

--- src/components/Profile/Dialogs/Dialogs.jsx
+++ src/components/Profile/Dialogs/Dialogs.jsx
@@ -3,17 +3,33 @@ import styles from "./Dialogs.module.css"
 import Message from "./Message/Message"
 import DialogItem from "./DialogItem/DialogItem"
 
+
 const Dialogs = (props) => {
 
-    let dialogsElement=props.dialogsData.map(d=><DialogItem name={d.name} id={d.id} />)
-    let messagesElement=props.messagesData.map(m=><Message message={m.message} />)
+    let dialogsElement = props.dialogsData.map(d => <DialogItem name={d.name} id={d.id} />)
+    let messagesElement = props.messagesData.map(m => <Message message={m.message} />)
+
+    let newMessage = React.createRef()
+    let newMess = () => {
+        let text = newMessage.current.value
+        alert(text)
+    }
+
     return (
         <div className={styles.dialogs}>
             <div>
                 {dialogsElement}
-        </div>
-        <div>
+            </div>
+            <div>
                 {messagesElement}
+            </div>
+            <div>
+                <div>
+                    <textarea ref={newMessage}></textarea>
+                </div>
+                <div> 
+                    <button onClick={newMess}> push message</button>
+                </div>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,9 +5,19 @@ let postsElement=props.posts.map(posts=>(<div><div className={styles.post}>
     {posts.message}</div>
     <div className={styles.likes}> Likes:{posts.like}</div>
 </div>))
+let newPostElement=React.createRef()
+let newPost=()=>{
+   let text=newPostElement.current.value
+    alert(text)
+} 
+    
     return <div>
+        
         <div>
-            New posts
+            <textarea ref={newPostElement}></textarea>
+            <div>
+                <button onClick={newPost}> Add post</button>
+            </div>
         </div>
        {postsElement}
        
