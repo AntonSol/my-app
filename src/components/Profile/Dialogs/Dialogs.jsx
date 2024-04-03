58ff97a2106f233f7ecee87f310a50a4193e59df
AntonS <solukov.anton@gmail.com>
2024-04-03 20:19:03 +0300

    1

--- src/components/Profile/Dialogs/Dialogs.jsx
+++ src/components/Profile/Dialogs/Dialogs.jsx
@@ -1,37 +1,41 @@
-import React from "react"
-import styles from "./Dialogs.module.css"
-import Message from "./Message/Message"
-import DialogItem from "./DialogItem/DialogItem"
-
+import React from "react";
+import styles from "./Dialogs.module.css";
+import Message from "./Message/Message";
+import DialogItem from "./DialogItem/DialogItem";
 
 const Dialogs = (props) => {
+  // Проверяем, есть ли объект store в props, и существуют ли необходимые данные
+  if (!props.store || !props.store._state || !props.store._state.messagePage) {
+    return <div>No data</div>;
+  }
 
-    let dialogsElement = props.dialogsData.map(d => <DialogItem name={d.name} id={d.id} />)
-    let messagesElement = props.messagesData.map(m => <Message message={m.message} />)
+  const dialogsElement = props.store._state.messagePage.dialogsData.map((d) => (
+    <DialogItem key={d.id} name={d.name} id={d.id} />
+  ));
+  const messagesElement = props.store._state.messagePage.messagesData.map(
+    (m) => <Message key={m.id} message={m.message} />
+  );
 
-    let newMessage = React.createRef()
-    let newMess = () => {
-        let text = newMessage.current.value
-        alert(text)
-    }
+  let newMessage = React.createRef();
+  const newMess = () => {
+    let text = newMessage.current.value;
+    alert(text);
+  };
 
-    return (
-        <div className={styles.dialogs}>
-            <div>
-                {dialogsElement}
-            </div>
-            <div>
-                {messagesElement}
-            </div>
-            <div>
-                <div>
-                    <textarea ref={newMessage}></textarea>
-                </div>
-                <div> 
-                 <button onClick={newMess}> push message</button>
-                </div>
-            </div>
+  return (
+    <div className={styles.dialogs}>
+      <div>{dialogsElement}</div>
+      <div>{messagesElement}</div>
+      <div>
+        <div>
+          <textarea ref={newMessage}></textarea>
         </div>
-    )
-}
-export default Dialogs
+        <div>
+          <button onClick={newMess}> push message</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialogs;
